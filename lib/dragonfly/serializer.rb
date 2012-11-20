13c25f824f5dc1920339b7e5f6125bd4324a77f4
Levin Alexander <mail@levinalex.net>
2012-11-20 20:33:17 +0100

    avoid "~" in generated urls, use '_'

--- lib/dragonfly/serializer.rb
+++ lib/dragonfly/serializer.rb
@@ -3,30 +3,30 @@ require 'base64'
 
 module Dragonfly
   module Serializer
-    
+
     # Exceptions
     class BadString < RuntimeError; end
-    
+
     extend self # So we can do Serializer.b64_encode, etc.
-    
+
     def b64_encode(string)
-      Base64.encode64(string).tr("\n=",'').tr('/','~')
+      Base64.encode64(string).tr("\n=",'').tr('/','_')
     end
-    
+
     def b64_decode(string)
       padding_length = string.length % 4
-      Base64.decode64(string.tr('~','/') + '=' * padding_length)
+      Base64.decode64(string.tr('~_','/') + '=' * padding_length)
     end
-    
+
     def marshal_encode(object)
       b64_encode(Marshal.dump(object))
     end
-    
+
     def marshal_decode(string)
       Marshal.load(b64_decode(string))
     rescue TypeError, ArgumentError => e
       raise BadString, "couldn't decode #{string} - got #{e}"
     end
-    
+
   end
 end
